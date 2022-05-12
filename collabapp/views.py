7917d12708d00130ee948688e8e312f6d9f224b9
cmcaquilala <cmcaquilala@up.edu.ph>
2022-05-13 07:20:28 +0800

    set up settings.py, urls.py, views.py

--- collabapp/views.py
+++ collabapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# def index(request):
+#     return render(request, 'catalog/dashboard.html')
+
+def loginPage(request):
+    return render(request, 'collabapp/hello.html')
