3dad3dadd09aca564be7a6f77f298974f1aa8177
Cedric Caquilala <82787259+cmcaquilala@users.noreply.github.com>
2022-05-14 19:58:16 +0800

    Front cedric (#1)
    * added html templates, dashboard
    
    * added login, project, account settings, some links
    
    * added edit-task, project-home; edited some links

--- collabapp/urls.py
+++ collabapp/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.indexPage, name='indexPage'),
+    path('dashboard', views.dashboardPage, name='dashboardPage'),
     path('login', views.loginPage, name='loginPage'),
+    path('account', views.accountPage, name='accountPage'),
+    path('project', views.projectPage, name='projectPage'),
+    path('projectInfo', views.projectInfoPage, name='projectInfoPage'),
+    path('task', views.taskPage, name='taskPage'),
 ]
--- collabapp/views.py
+++ collabapp/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 # def index(request):
 #     return render(request, 'catalog/dashboard.html')
 
+def indexPage(request):
+    # if not logged in
+    # return render(request, 'collabapp/login.hmtl')
+
+    # if logged in:
+    return redirect('dashboardPage')
+
+def dashboardPage(request):
+    return render(request, 'collabapp/dashboard.html')
+
 def loginPage(request):
-    return render(request, 'collabapp/hello.html')
+    return render(request, 'collabapp/login.html')
+
+def accountPage(request):
+    return render(request, 'collabapp/account.html')
+
+def projectPage(request):
+    return render(request, 'collabapp/project-home.html')
+
+def projectInfoPage(request):
+    return render(request, 'collabapp/project-info.html')
+
+def taskPage(request):
+    return render(request, 'collabapp/edit-task.html')
