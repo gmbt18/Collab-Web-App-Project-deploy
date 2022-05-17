52956801b75c80432f9430d7a61faec48fa288fc
Emil Paulo R. Mallorca <kobe.mallorca@gmail.com>
2022-05-17 16:37:40 +0800

    crispy sa htmls
    wala pa sa mismong forms

--- collabapp/models.py
+++ collabapp/models.py
@@ -6,9 +6,6 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-
-
 class Project(models.Model):
     code = models.CharField(max_length=255, null=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null = True)
