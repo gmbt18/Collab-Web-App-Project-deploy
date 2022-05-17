031b504fef6108c22fe3a39668021543a7d8ca6f
Cedric Caquilala <82787259+cmcaquilala@users.noreply.github.com>
2022-05-17 22:59:38 +0800

    Favicons, Titles, Forms (#15)
    * head edits: added icon, edited page titles
    
    * cleaned up forms, fixed login and register pages

--- collabapp/forms.py
+++ collabapp/forms.py
@@ -15,17 +15,33 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title']
 
+        labels = {
+            'title' : 'Project Name'
+        }
+
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'due_date']
 
+        labels = {
+            'name' : 'Task Name',
+            'due_date' : 'Due Date'
+        }
+
 class EditUserForm(UserChangeForm):
     class Meta:
         model = User
         fields = ['email','password']
+        labels = {
+            'email' : 'Email Address'
+        }
 
 class EditProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['name','contact','projects']
+
+        labels = {
+            'contact' : 'Contact No.',
+        }
